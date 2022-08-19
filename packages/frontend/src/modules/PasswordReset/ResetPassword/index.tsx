28ed5b90ac4c8dabeb6caa090e2688fa17b285dd
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2022-08-19 10:26:51 -0700

    Refining password message to give more clear instructions

--- packages/frontend/src/modules/PasswordReset/ResetPassword/index.tsx
+++ packages/frontend/src/modules/PasswordReset/ResetPassword/index.tsx
@@ -54,7 +54,13 @@ export default function ResetPassword() {
                   if (e.target.value.length < 8) {
                     setError('Password must be at least 8 characters');
                   } else if (!StringUtils.isValidPassword(e.target.value)) {
-                    setError('Password must contain at least one letter and one number');
+                    const hasLetter = StringUtils.containsLetter(e.target.value);
+                    const hasNumber = StringUtils.containsNumber(e.target.value);
+                    setError(
+                      `Password must contain at least ${!hasLetter ? 'one letter' : ''}${
+                        !hasLetter && !hasNumber ? ' and ' : ''
+                      }${!hasNumber ? 'one number' : ''}`
+                    );
                   }
                   setNewPassword(e.target.value);
                 }}
